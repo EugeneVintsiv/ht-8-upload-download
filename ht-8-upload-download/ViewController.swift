47fe81a8eaa47c11fc002a8078ddd67f8a680020
Eugene <eugene.vintsiv@gmail.com>
2019-01-17 18:45:23 +0200

    Initial Commit

---
+++ ht-8-upload-download/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ht-8-upload-download
+//
+//  Created by Eugene on 1/17/19.
+//  Copyright © 2019 Eugene. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
