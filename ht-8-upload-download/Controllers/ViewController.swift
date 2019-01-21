297973f18ae4a3e3349c31d471b87187b7e960be
Eugene <eugene.vintsiv@gmail.com>
2019-01-21 17:45:19 +0200

    fix issue with downloading file, add percent progress

--- ht-8-upload-download/Controllers/ViewController.swift
+++ ht-8-upload-download/Controllers/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var extractVideoLinkBtn: UIButton!
     @IBOutlet weak var videoLinkLabel: UILabel!
     @IBOutlet weak var progressView: UIProgressView!
+    @IBOutlet weak var progressPercent: UILabel!
     
     @IBAction func onUploadButtonClick(_ sender: Any) {
         uploadImageBtn.isEnabled = false
@@ -27,38 +28,24 @@ class ViewController: UIViewController {
             self.uploadImageBtn.isEnabled = true
         }
     }
-    let documentsPath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
 
-    private lazy var downloadsSession : URLSession = {
-//        let config = URLSessionConfiguration.background(withIdentifier: "\(Bundle.main.bundleIdentifier!).background")
-        let config = URLSessionConfiguration.default
-
-            // Warning: If an URLSession still exists from a previous download, it doesn't create
-            // a new URLSession object but returns the existing one with the old delegate object attached!
-        return URLSession(configuration: config, delegate: self, delegateQueue: OperationQueue())
-    }()
-
-    let tmpLink = "https://media1.giphy.com/media/2SXRjZG012DlYRRBT8/giphy.mp4"
     @IBAction func onExtractVideoLinkClicked(_ sender: Any) {
         extractVideoLinkBtn.isEnabled = false
         videoLinkLabel.isHidden = true
-
-        imageService.downloadFileById(fileId: imageIdLabel.text) { (link) in
-            self.videoLinkLabel.text = link
-            self.videoLinkLabel.isHidden = false
-            self.extractVideoLinkBtn.isEnabled = true
-            self.progressView.progress = 0.0
-
-            print(link)
-//            self.downloadsSession.invalidateAndCancel()
-
-            self.progressView.isHidden = false
-            self.downloadsSession.dataTask(with: URL(string: link!)!).resume()
-        }
+        downloadFile()
     }
 
 //    DI
     private lazy var imageService: ImageService = ImageServiceImpl()
+    private let documentsPath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+    private lazy var downloadsSession : URLSession = {
+        let config = URLSessionConfiguration.default
+        // Warning: If an URLSession still exists from a previous download, it doesn't create
+        // a new URLSession object but returns the existing one with the old delegate object attached!
+        return URLSession(configuration: config, delegate: self, delegateQueue: OperationQueue())
+    }()
+
+    private let tmpLink = "https://media1.giphy.com/media/2SXRjZG012DlYRRBT8/giphy.mp4"
 
     override func viewDidLoad() {
         urlLinkTextInput.text = "https://loading.io/spinners/spiral/lg.rotate-spiral-spinner.gif"
@@ -69,10 +56,17 @@ class ViewController: UIViewController {
         super.viewDidLoad()
     }
 
-    private func downloadFile(fileLink url: String?) {
-        if url == nil {
-//            may show error
-            return
+    private func downloadFile() {
+        imageService.downloadFileById(fileId: imageIdLabel.text) { (link) in
+            self.videoLinkLabel.text = link
+            self.videoLinkLabel.isHidden = false
+            self.extractVideoLinkBtn.isEnabled = true
+            self.progressView.progress = 0.0
+            self.progressPercent.isHidden = false
+            self.progressPercent.text = "0"
+
+            self.progressView.isHidden = false
+            self.downloadsSession.downloadTask(with: URL(string: link!)!).resume()
         }
     }
 
@@ -80,7 +74,6 @@ class ViewController: UIViewController {
 
 extension ViewController: URLSessionDownloadDelegate {
     func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
-        print("Download finished ")
         guard let sourceURL = downloadTask.originalRequest?.url else { return }
         let destinationURL = localFilePath(for: sourceURL)
         print(destinationURL)
@@ -88,24 +81,21 @@ extension ViewController: URLSessionDownloadDelegate {
         try? fileManager.removeItem(at: destinationURL)
         do {
             try fileManager.copyItem(at: location, to: destinationURL)
-            print("Download Finished")
         } catch let error {
             print("Could not copy file to disk: \(error.localizedDescription)")
         }
-        print("Finished")
     }
     
     func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didWriteData bytesWritten: Int64, totalBytesWritten: Int64, totalBytesExpectedToWrite: Int64) {
-        print("In progress: \(totalBytesWritten)")
         guard totalBytesExpectedToWrite != NSURLSessionTransferSizeUnknown else {
             return
         }
 
         let progress = Float(Double(totalBytesWritten)/Double(totalBytesExpectedToWrite))
-        print("Download progress: \(progress)")
-
-        print(progress)
-        DispatchQueue.main.async { self.progressView.setProgress(progress, animated: true) }
+        DispatchQueue.main.async {
+            self.progressView.setProgress(progress, animated: true)
+            self.progressPercent.text = String(format: "%.1f%%", progress * 100)
+        }
     }
 
     func localFilePath(for url: URL) -> URL {
